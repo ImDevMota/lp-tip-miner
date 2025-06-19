041d24c4aff7f824fd643299863127fedd1b32db
Thiago Mota <thiago.you23@gmail.com>
2025-06-18 22:33:43 -0300

    Desktop: Pequenas alterações na section outdoor

--- src/components/Outdoor.jsx
+++ src/components/Outdoor.jsx
@@ -19,16 +19,16 @@ function Outdoor() {
           style={{ backgroundImage: `url(${OutdoorBgDk})` }}
         />
 
-        <div className="relative z-10 flex sm:w-[80%] flex-col sm:items-start items-center mt-[3rem] sm:mt-[5rem]">
+        <div className="relative z-10 flex sm:w-[70%] flex-col sm:items-start items-center mt-[3rem] sm:mt-[5rem]">
           {/* Imagem não precisa ser absolute — apenas ajustada no layout */}
 
           <img src={Logo} alt="Logo" className="sm:hidden" />
           <img src={LogoDk} alt="Logo" className="sm:block hidden" />
 
-          <div className="flex flex-col sm:w-[40%] w-[85%] mt-[25.8rem] sm:mt-[9rem]">
-            <h2 className="text-white text-center sm:text-start leading-tight text-[24px] sm:text-[44px] font-[600]">
+          <div className="flex flex-col sm:w-[40%] w-[85%] mt-[25.8rem] sm:mt-[9rem] sm:whitespace-nowrap">
+            <h2 className="text-white text-center sm:text-start leading-tight  text-[24px] sm:text-[44px] font-[600]">
               Você vai se tornar o pior <br className="hidden sm:block" />{" "}
-              pesadelo dos cassinos online,{" "}
+              pesadelo dos cassinos online, <br />
               <span className="text-[23px] sm:text-[44px] font-[600] text-center leading-tight text-transparent bg-clip-text bg-gradient-to-r from-[#03BDE3] to-[#0515E7]">
                 enchendo o bolso de dinheiro
               </span>
@@ -64,7 +64,7 @@ function Outdoor() {
               </p>
             </div>
 
-            <p className="text-[16px] font-[400] text-[white] sm:text-[20px] sm:text-start text-center mt-[1.5rem]">
+            <p className="text-[16px] font-[400] text-[white] sm:text-[20px] sm:whitespace-nowrap sm:text-start text-center mt-[1.5rem]">
               Tenha em mãos o sistema exclusivo do TipMiner para{" "}
               <br className="hidden sm:block" /> dominar os resultados dos jogos
               online. Garanta seus <br className="hidden sm:block" />
@@ -75,7 +75,7 @@ function Outdoor() {
           </div>
 
           {/* Botão */}
-          <a className="text-[white] mt-[2.5rem] text-[14px] sm:w-[32%] sm:py-[1.1rem] sm:text-[20px] w-[85%] text-center px-[1.3rem] py-[0.8rem] rounded-md font-[600] bg-gradient-to-r from-[#02C1E3] to-[#0412E7]">
+          <a className="text-[white] mt-[2.5rem] text-[14px] sm:w-[29rem] sm:justify-center sm:flex sm:whitespace-nowrap sm:py-[1.1rem] sm:text-[20px] w-[85%] text-center px-[1.3rem] py-[0.8rem] rounded-md font-[600] bg-gradient-to-r from-[#02C1E3] to-[#0412E7]">
             Quero aumentar meus lucros com o TipMiner
           </a>
 
@@ -86,14 +86,17 @@ function Outdoor() {
             className="mt-[4rem] w-full block sm:hidden"
           />
 
-          <img
-            src={LogosBetsDk}
-            alt="Parceiros"
-            className="mt-[15rem] hidden sm:block"
-          />
+          <div class="relative w-full h-[10rem] flex justify-center items-center top-[10rem]">
+            <img
+              src={LogosBetsDk}
+              alt="Parceiros"
+              className="absolute left-1/2 transform -translate-x-1/2 hidden sm:w-full sm:block"
+            />
+            <div class="pointer-events-none absolute inset-0 bg-gradient-to-r from-[#080126] via-white/0 via-70% to-[#080126]" />
+          </div>
 
           {/* Bloco informativo */}
-          <div className="p-[1.5px] bg-gradient-to-b from-[#5F858E] to-[#100a2e] rounded-2xl w-[85%] mt-[4rem]">
+          <div className="p-[1.5px] bg-gradient-to-b from-[#5F858E] to-[#100a2e] rounded-2xl w-[85%] sm:mt-[15rem] mt-[4rem]">
             <div className="bg-[#100a2e] backdrop-blur-md py-[3rem] rounded-2xl text-[white] text-center flex flex-col justify-center items-center">
               <div className="flex flex-col w-[85%]">
                 <h2 className="font-[600] text-[32px]">
