27aed3b87b1d43eeadd23dd06cb3cd05185cbc2f
Thiago Mota <thiago.you23@gmail.com>
2025-06-27 15:08:06 -0300

    Desktop: Finalizei a section differentials

--- src/components/Differentials.jsx
+++ src/components/Differentials.jsx
@@ -3,113 +3,145 @@ import CompassImg from "../assets/compass.svg";
 import DatabaseImg from "../assets/database.svg";
 import RunImg from "../assets/run.svg";
 import Elipse2Img from "../assets/elipse-2.png";
+import Elipse2DesktopImg from "../assets/elipse-2-desktop.png";
 
 function Differentials() {
   return (
     <section className="mt-[2rem] flex justify-center items-center relative">
-      <div className="flex flex-col w-[85%] mb-[3rem]">
-        <h2 className="text-white text-[24px] font-[700] text-center">
+      <div className="flex flex-col w-[85%] mb-[3rem] sm:items-center">
+        <h2 className="text-white text-[24px] sm:text-[56px] font-[700] text-center">
           Quais São Os Diferenciais do TipMiner:
         </h2>
 
-        <div className="flex flex-col items-center justify-center mt-[2rem] gap-y-[1rem]">
-          <div className="p-[1px] bg-[#FFFFFF4D] rounded-2xl w-[100%] overflow-hidden">
-            <div className="bg-[#100a2e]/90 h-[13.5rem] backdrop-blur-md py-[1rem] items-center justify-center rounded-2xl text-[white] flex flex-col">
-              <div className="flex flex-col absolute top-[-0.1rem] left-[1rem] h-[0.8rem] items-center">
-                <div class="w-[2.5rem] h-0.5 bg-[#02C1E3]"></div>
-                <div class="absolute top-[-1.2rem] left-0 w-[2.35rem] h-[2.35rem] bg-[#02C1E3] rounded-full blur-[0.6rem] opacity-25"></div>
-              </div>
-              <div className="flex flex-col w-[82%] h-[12.3rem] justify-center">
-                <div className="flex rounded-full w-fit border-[0.12rem] border-[#FFFFFF4D]">
-                  <img src={CompassImg} alt="" className="w-[38px] h-[38px]" />
+        <div className="flex flex-col sm:flex-row items-center justify-center mt-[2rem] sm:mt-[3.6rem] gap-y-[1rem] sm:gap-x-[1rem]">
+          <div className="flex flex-col gap-y-[1rem] sm:gap-y-[1.5rem]">
+            <div className="p-[1px] bg-[#FFFFFF4D] rounded-3xl w-[100%] sm:w-[33rem]  overflow-hidden">
+              <div className="bg-[#100a2e]/90 h-[13.5rem] sm:h-[19.6rem] backdrop-blur-md py-[1rem] items-center justify-center rounded-3xl text-[white] flex flex-col">
+                <div className="flex flex-col absolute top-[-0.1rem] left-[1rem] sm:left-[1.55rem] h-[0.8rem] items-center">
+                  <div class="w-[2.5rem] sm:w-[3rem] h-0.5 bg-[#02C1E3]"></div>
+                  <div class="absolute top-[-1.2rem] left-0 w-[2.35rem] sm:w-[2.75rem] sm:h-[2.75rem] h-[2.35rem] bg-[#02C1E3] rounded-full blur-[0.6rem] opacity-25"></div>
                 </div>
-                <p className="text-white text-[12.9px] font-[300] mt-[1.2rem]">
-                  <span className="font-bold">Precisão Analítica: </span>
-                  Imagine ter uma bússola dos jogos ao seu lado, guiando cada
-                  aposta com insights gerados por dados avançados, sobre quando,
-                  onde e como apostar para ganhar.
-                </p>
+
+                <div className="flex flex-col w-[82%] h-[12.3rem] justify-center">
+                  <div className="flex rounded-full w-fit border-[0.12rem] sm:border-[0.2rem] sm:p-[0.24rem] border-[#FFFFFF4D]">
+                    <img
+                      src={CompassImg}
+                      alt=""
+                      className="w-[38px] h-[38px] sm:w-[48px] sm:h-[48px]"
+                    />
+                  </div>
+                  <p className="text-white text-[12.9px] sm:text-[20px] font-[300] mt-[1.2rem]">
+                    <span className="font-bold">Precisão Analítica: </span>
+                    Imagine ter uma bússola dos jogos ao seu lado, guiando cada
+                    aposta com insights gerados por dados avançados, sobre
+                    quando, onde e como apostar para ganhar.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="p-[1px] bg-[#FFFFFF4D] rounded-3xl w-[100%] sm:w-[33rem]  overflow-hidden">
+              <div className="bg-[#100a2e]/90 h-[13.5rem] sm:h-[19.6rem] backdrop-blur-md py-[1rem] relative items-center justify-center rounded-3xl text-[white] flex flex-col">
+                <div className="flex flex-col absolute top-[-0.1rem] left-[1rem] sm:left-[1.55rem] h-[0.8rem] items-center">
+                  <div class="w-[2.5rem] sm:w-[3rem] h-0.5 bg-[#02C1E3]"></div>
+                  <div class="absolute top-[-1.2rem] left-0 w-[2.35rem] sm:w-[2.75rem] sm:h-[2.75rem] h-[2.35rem] bg-[#02C1E3] rounded-full blur-[0.6rem] opacity-25"></div>
+                </div>
+
+                <div className="flex flex-col h-[12.3rem] w-[82%] justify-center relative">
+                  <div className="flex rounded-full w-fit p-[0.62rem] sm:p-[0.93rem] border-[0.12rem] sm:border-[0.2rem] border-[#FFFFFF4D]">
+                    <img
+                      src={DatabaseImg}
+                      alt=""
+                      className="w-[18.5px] h-[18.5px] sm:w-[25.8px] sm:h-[25.8px]"
+                    />
+                  </div>
+                  <p className="text-white text-[12.9px] sm:text-[20px] font-[300] mt-[1.2rem]">
+                    <span className="font-bold">
+                      Análise Avançada de Jogos:{" "}
+                    </span>
+                    O TipMiner desvenda os segredos dos maiores dos jogos
+                    online, oferecendo análises tão profundas que você sentirá
+                    como se pudesse prever o futuro. Cada movimento é uma jogada
+                    calculada rumo à riqueza.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
 
-          <div className="p-[1px] bg-[#FFFFFF4D] rounded-2xl w-[100%] overflow-hidden">
-            <div className="bg-[#100a2e]/90 h-[13.5rem] backdrop-blur-md py-[1rem] relative items-center justify-center rounded-2xl text-[white] flex flex-col">
-              <div className="flex flex-col absolute top-[-0.1rem] left-[1rem] h-[0.8rem] items-center">
-                <div class="w-[2.5rem] h-0.5 bg-[#02C1E3]"></div>
-                <div class="absolute top-[-1.2rem] left-0 w-[2.35rem] h-[2.35rem] bg-[#02C1E3] rounded-full blur-[0.6rem] opacity-25"></div>
-              </div>
-
-              <div className="flex flex-col h-[12.3rem] w-[82%] justify-center relative">
-                <div className="flex rounded-full w-fit p-[0.62rem] border-[0.12rem] border-[#FFFFFF4D]">
-                  <img
-                    src={DatabaseImg}
-                    alt=""
-                    className="w-[18.5px] h-[18.5px]"
-                  />
+          <div className="flex flex-col gap-y-[1rem] sm:gap-y-[1.5rem]">
+            <div className="p-[1px] bg-[#FFFFFF4D] rounded-3xl w-[100%] sm:w-[33rem]  overflow-hidden">
+              <div className="bg-[#100a2e]/90 h-[13.5rem] sm:h-[19.6rem] backdrop-blur-md py-[1rem] items-center justify-center rounded-3xl text-[white] flex flex-col">
+                <div className="flex flex-col absolute top-[-0.1rem] left-[1rem] sm:left-[1.55rem] h-[0.8rem] items-center">
+                  <div class="w-[2.5rem] sm:w-[3rem] h-0.5 bg-[#02C1E3]"></div>
+                  <div class="absolute top-[-1.2rem] left-0 w-[2.35rem] sm:w-[2.75rem] sm:h-[2.75rem] h-[2.35rem] bg-[#02C1E3] rounded-full blur-[0.6rem] opacity-25"></div>
                 </div>
-                <p className="text-white text-[12.9px] font-[300] mt-[1.2rem]">
-                  <span className="font-bold">Análise Avançada de Jogos: </span>
-                  O TipMiner desvenda os segredos dos maiores dos jogos online,
-                  oferecendo análises tão profundas que você sentirá como se
-                  pudesse prever o futuro. Cada movimento é uma jogada calculada
-                  rumo à riqueza.
-                </p>
+
+                <div className="flex flex-col w-[82%] h-[12.3rem] justify-center">
+                  <div className="flex rounded-full w-fit p-[0.5rem] sm:p-[0.644rem] border-[0.12rem] sm:border-[0.2rem] border-[#FFFFFF4D]">
+                    <LaptopMinimalCheck
+                      size={23}
+                      className="text-[#02C1E3] sm:hidden block"
+                    />
+
+                    <LaptopMinimalCheck
+                      size={35}
+                      className="text-[#02C1E3] hidden sm:block"
+                    />
+                  </div>
+                  <p className="text-white text-[12.9px] sm:text-[20px] font-[300] mt-[1.2rem]">
+                    <span className="font-bold">Excelência do Software: </span>O
+                    TipMiner não é apenas um software qualquer; é uma ferramenta
+                    desenvolvida com o máximo de excelência e eficácia,
+                    desenhada para acertar os resultados com base em dados e
+                    análise de padrões.
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="p-[1px] bg-[#FFFFFF4D] rounded-2xl w-[100%] overflow-hidden">
-            <div className="bg-[#100a2e]/90 h-[13.5rem] backdrop-blur-md py-[1rem] items-center justify-center rounded-2xl text-[white] flex flex-col">
-              <div className="flex flex-col absolute top-[-0.1rem] left-[1rem] h-[0.8rem] items-center">
-                <div class="w-[2.5rem] h-0.5 bg-[#02C1E3]"></div>
-                <div class="absolute top-[-1.2rem] left-0 w-[2.35rem] h-[2.35rem] bg-[#02C1E3] rounded-full blur-[0.6rem] opacity-25"></div>
-              </div>
-              <div className="flex flex-col w-[82%] h-[12.3rem] justify-center">
-                <div className="flex rounded-full w-fit p-[0.5rem] border-[0.12rem] border-[#FFFFFF4D]">
-                  <LaptopMinimalCheck size={23} className="text-[#02C1E3]" />
+            <div className="p-[1px] bg-[#FFFFFF4D] rounded-3xl w-[100%] sm:w-[33rem]  overflow-hidden">
+              <div className="bg-[#100a2e]/90 h-[13.5rem] sm:h-[19.6rem] backdrop-blur-md py-[1rem] items-center justify-center rounded-3xl text-[white] flex flex-col">
+                <div className="flex flex-col absolute top-[-0.1rem] left-[1rem] sm:left-[1.55rem] h-[0.8rem] items-center">
+                  <div class="w-[2.5rem] sm:w-[3rem] h-0.5 bg-[#02C1E3]"></div>
+                  <div class="absolute top-[-1.2rem] left-0 w-[2.35rem] sm:w-[2.75rem] sm:h-[2.75rem] h-[2.35rem] bg-[#02C1E3] rounded-full blur-[0.6rem] opacity-25"></div>
                 </div>
-                <p className="text-white text-[12.9px] font-[300] mt-[1.2rem]">
-                  <span className="font-bold">Excelência do Software: </span>O
-                  TipMiner não é apenas um software qualquer; é uma ferramenta
-                  desenvolvida com o máximo de excelência e eficácia, desenhada
-                  para acertar os resultados com base em dados e análise de
-                  padrões.
-                </p>
-              </div>
-            </div>
-          </div>
 
-          <div className="p-[1px] bg-[#FFFFFF4D] rounded-2xl w-[100%] overflow-hidden">
-            <div className="bg-[#100a2e]/90 h-[13.5rem] backdrop-blur-md py-[1rem] items-center justify-center rounded-2xl text-[white] flex flex-col">
-              <div className="flex flex-col absolute top-[-0.1rem] left-[1rem] h-[0.8rem] items-center">
-                <div class="w-[2.5rem] h-0.5 bg-[#02C1E3]"></div>
-                <div class="absolute top-[-1.2rem] left-0 w-[2.35rem] h-[2.35rem] bg-[#02C1E3] rounded-full blur-[0.6rem] opacity-25"></div>
-              </div>
-              <div className="flex flex-col w-[82%] h-[12.3rem] justify-center">
-                <div className="flex rounded-full w-fit p-[0.5rem] border-[0.12rem] border-[#FFFFFF4D]">
-                  <img src={RunImg} alt="" className="w-[24px] h-[24px]" />
+                <div className="flex flex-col w-[82%] h-[12.3rem] justify-center">
+                  <div className="flex rounded-full w-fit p-[0.5rem] sm:p-[0.71rem] border-[0.12rem] sm:border-[0.2rem] border-[#FFFFFF4D]">
+                    <img
+                      src={RunImg}
+                      alt=""
+                      className="w-[24px] h-[24px] sm:w-[33px] sm:h-[33px]"
+                    />
+                  </div>
+                  <p className="text-white text-[12.9px] sm:text-[20px] font-[300] mt-[1.2rem]">
+                    <span className="font-bold">Automatização e Rapidez: </span>
+                    No mundo dos jogos online, a velocidade é tão crucial quanto
+                    a estratégia. Com o TipMiner, você está sempre um passo à
+                    frente, graças à nossa plataforma ágil que automatiza a
+                    análise e a tomada de decisões.
+                  </p>
                 </div>
-                <p className="text-white text-[12.9px] font-[300] mt-[1.2rem]">
-                  <span className="font-bold">Automatização e Rapidez: </span>
-                  No mundo dos jogos online, a velocidade é tão crucial quanto a{" "}
-                  estratégia. Com o TipMiner, você está sempre um passo à
-                  frente, graças à nossa plataforma ágil que automatiza a
-                  análise e a tomada de decisões.
-                </p>
               </div>
             </div>
           </div>
         </div>
 
-        <a className="text-[white] mt-[1rem] text-[14px] text-center px-[1.3rem] py-[0.8rem] rounded-md font-[600] bg-gradient-to-r from-[#02C1E3] to-[#0412E7]">
+        <a className="text-[white] mt-[2.5rem] sm:mt-[6rem] sm:mb-[2.5rem] text-[14px] sm:w-[32.35rem] sm:justify-center sm:flex sm:whitespace-nowrap sm:py-[1.1rem] sm:text-[20px] w-[85%] text-center px-[1.3rem] py-[0.8rem] rounded-md font-[600] bg-gradient-to-r from-[#02C1E3] to-[#0412E7]">
           Quero aumentar meus lucros com o TipMiner
         </a>
 
         <img
           src={Elipse2Img}
           alt=""
-          className="absolute left-0 bottom-2 w-[100%]"
+          className="absolute left-0 bottom-2 w-[100%] sm:hidden block"
+        />
+
+        <img
+          src={Elipse2DesktopImg}
+          alt=""
+          className="absolute left-0 bottom-2 w-[100%] sm:block hidden"
         />
       </div>
     </section>
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -5,41 +5,40 @@ import ImgPessoa2 from "../assets/pessoa-2.png";
 function Features() {
   return (
     <section className="flex justify-center items-center bg-white mt-[2rem]">
-      <div className="flex flex-col items-center justify-center w-[85%] mt-[2.5rem]">
-        <div className="relative w-fit rounded-lg overflow-hidden">
-          <img
-            src={ImgPessoa1}
-            alt=""
-            className="w-[292px] h-[362px] drop-shadow-[0_20px_15px_rgba(0,0,0,0.1)]"
-          />
-        </div>
+      <div className="flex flex-col items-center justify-center w-[85%] sm:w-[70%] mt-[2.5rem]">
+        <div className="flex flex-col sm:flex-row sm:w-[80%] sm:justify-between items-center sm:items-start">
+          <div className="relative w-fit sm:order-2 rounded-lg overflow-hidden">
+            <img
+              src={ImgPessoa1}
+              alt=""
+              className="w-[292px] h-[362px] sm:w-[414px] sm:h-[514px] drop-shadow-[0_20px_15px_rgba(0,0,0,0.1)]"
+            />
+          </div>
 
-        <div className="flex flex-col mt-[2rem]">
-          <h2 className="text-[27px] font-[700] text-black">
-            Para Quem é o TipMiner:
-          </h2>
-
-          <div className="flex flex-col ml-[0.3rem] gap-y-4 mt-[1.5rem]">
-            <div className="flex flex-row gap-x-[1rem] items-center">
-              <Check className="text-[#02C1E3]" />
-              <p className="text-[14.5px] font-[500]">
+          <div className="flex flex-col ml-[0.3rem] gap-y-4 mt-[2rem]">
+            <h2 className="text-[27px] sm:text-[48px] font-[700] text-black ">
+              Para Quem é o TipMiner:
+            </h2>
+            <div className="flex flex-row gap-x-[1rem] sm:gap-x-[2rem] sm:mt-[1.5rem] items-center">
+              <Check className="text-[#02C1E3] sm:size-[32px]" />
+              <p className="text-[14.5px] sm:text-[24px] font-[500] ">
                 Para quem busca ter uma renda extra, com <br />
                 menos esforço e mais assertividade
               </p>
             </div>
 
-            <div className="flex flex-row gap-x-[1rem] items-center">
-              <Check className="text-[#02C1E3]" />
-              <p className="text-[14.5px] font-[500] text-start">
+            <div className="flex flex-row gap-x-[1rem] sm:gap-x-[2rem] items-center">
+              <Check className="text-[#02C1E3] sm:size-[32px]" />
+              <p className="text-[14.5px] sm:text-[24px] font-[500]  text-start">
                 Para Jogadores Casuais que desejam <br />
                 aumentar seus lucros sem precisar gastar <br />
                 horas em análise e pesquisa.
               </p>
             </div>
 
-            <div className="flex flex-row gap-x-[1rem] items-center">
-              <Check className="text-[#02C1E3]" />
-              <p className="text-[14.5px] font-[500]">
+            <div className="flex flex-row gap-x-[1rem] sm:gap-x-[2rem] items-center">
+              <Check className="text-[#02C1E3] sm:size-[32px]" />
+              <p className="text-[14.5px] sm:text-[24px] font-[500] ">
                 Para Apostadores Profissionais que <br />
                 buscam uma vantagem competitiva e <br />
                 ferramentas para refinar suas estratégias <br />
@@ -49,35 +48,39 @@ function Features() {
           </div>
         </div>
 
-        <div className="mt-[4.5rem]">
-          <img src={ImgPessoa2} alt="" className="w-[292px] h-[362px]" />
-        </div>
+        <div className="flex flex-col sm:flex-row mb-[2rem] sm:w-[80%] sm:mb-[6rem] sm:justify-between items-center sm:mt-[5.5rem] mt-[4.5rem]">
+          <div className="">
+            <img
+              src={ImgPessoa2}
+              alt=""
+              className="w-[292px] h-[362px] sm:w-[414px] sm:h-[514px] rounded-[0.6rem]"
+            />
+          </div>
 
-        <div className="flex flex-col mt-[2rem] mb-[2rem]">
-          <h2 className="text-[25px] font-[700] text-black">
-            Pra quem não é o TipMiner:
-          </h2>
+          <div className="flex flex-col ml-[0.3rem] gap-y-5 mt-[2rem]">
+            <h2 className="text-[24.5px] sm:text-[48px] font-[700] text-black ">
+              Pra quem não é o TipMiner:
+            </h2>
 
-          <div className="flex flex-col ml-[0.3rem] gap-y-5 mt-[1.5rem]">
-            <div className="flex flex-row gap-x-[1rem] items-center">
-              <X className="text-[#FF0000]" />
-              <p className="text-[14.5px] font-[500]">
+            <div className="flex flex-row gap-x-[1rem] sm:gap-x-[2rem] sm:mt-[1.5rem] items-center">
+              <X className="text-[#FF0000] sm:size-[32px]" />
+              <p className="text-[14.5px] font-[500] sm:text-[24px] ">
                 Para quem Prefere confiar apenas na <br />
                 sorte.
               </p>
             </div>
 
-            <div className="flex flex-row gap-x-[1rem] items-center">
-              <X className="text-[#FF0000]" />
-              <p className="text-[14.5px] font-[500] text-start">
+            <div className="flex flex-row gap-x-[1rem] sm:gap-x-[2rem] items-center">
+              <X className="text-[#FF0000] sm:size-[32px]" />
+              <p className="text-[14.5px] font-[500] sm:text-[24px]  text-start">
                 Para quem Joga sem estratégia definida e <br />
                 não busca ferramentas aliadas.
               </p>
             </div>
 
-            <div className="flex flex-row gap-x-[1rem] items-center">
-              <X className="text-[#FF0000]" />
-              <p className="text-[14.5px] font-[500]">
+            <div className="flex flex-row gap-x-[1rem] sm:gap-x-[2rem] items-center">
+              <X className="text-[#FF0000] sm:size-[32px]" />
+              <p className="text-[14.5px] font-[500] sm:text-[24px] ">
                 Para quem Não tem responsabilidade com <br />
                 dinheiro e joga sem consciência.
               </p>
