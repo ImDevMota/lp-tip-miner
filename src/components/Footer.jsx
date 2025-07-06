e54517ab5cdbcbc698a64e7280b3fea2d056801e
Thiago Mota <thiago.you23@gmail.com>
2025-07-06 20:50:37 -0300

    Desktop: Finalizei o footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,27 +3,32 @@ import Logo from "../assets/logo-tip-miner.svg";
 function Footer() {
   return (
     <footer className="flex justify-center items-center border-t-[0.12rem] border-white bg-[#A1A1A10D]">
-      <div className="flex flex-col w-[85%] justify-center items-center mt-[3.5rem]">
-        <img src={Logo} alt="" className="w-[6.5rem]" />
+      <div className="flex flex-col sm:justify-between sm:flex-row sm:w-[65%] w-[85%] justify-center sm:items-start items-center mt-[3.5rem] sm:mb-[5.5rem]">
+        <img
+          src={Logo}
+          alt=""
+          className="w-[6.5rem] sm:w-[130px] sm:h-[60px] sm:mt-[2.4rem]"
+        />
 
-        <p className="text-white text-[20px] font-[600] text-center mt-[2.7rem]">
+        <p className="text-white text-[20px] font-[600] text-center sm:text-start mt-[2.7rem]">
           © 2024 - Todos os direitos reservados.{" "}
+          <br className="sm:block hidden" />
           <span className="font-[400]">Tipminer</span>
         </p>
 
-        <div className="flex flex-col mt-[2.5rem]">
-          <p className="text-white text-[20px] font-[600] text-center">
+        <div className="flex flex-col mt-[2.5rem] sm:text-start">
+          <p className="text-white text-[20px] font-[600] text-center sm:text-start">
             Precisa de ajuda?
           </p>
           <a
             href="#"
-            className="text-[#CDCDCD] text-[16px] font-[400] text-center"
+            className="text-[#CDCDCD] text-[16px] font-[400] text-center sm:text-start"
           >
             Termos de uso
           </a>
           <a
             href="#"
-            className="text-[#CDCDCD] text-[16px] font-[400] text-center"
+            className="text-[#CDCDCD] text-[16px] font-[400] text-center sm:text-start"
           >
             Política de privacidade
           </a>
